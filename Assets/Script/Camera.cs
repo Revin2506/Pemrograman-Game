bebd1bcccbfc416c91d4e94c25964336af763e6b
Revin2506 <revin.tanto98@gmail.com>
2020-06-30 17:07:33 +0700

    new

---
+++ Assets/Script/Camera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    public Transform player;
+    public float CameraDistance = 40.0f;
+
+    private void Awake()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / CameraDistance);
+    }
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public CharacterController2D controller;
+	public Animator animator;
+
+	public float runSpeed = 40f;
+
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool slide = false;
+
+	// Update is called once per frame
+	void Update () {
+
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+			animator.SetBool("IsJumping", true);
+		}
+
+		if (Input.GetButtonDown("Slide"))
+		{
+			slide = true;
+            animator.SetBool("IsSliding", true);
+        } else if (Input.GetButtonUp("Slide"))
+		{
+			slide = false;
+            animator.SetBool("IsSliding", false);
+        }
+
+	}
+
+	public void OnLanding ()
+	{
+		animator.SetBool("IsJumping", false);
+	}
+
+	//public void OnSliding (bool isSliding)
+	//{
+	//	animator.SetBool("IsSliding", isSliding);
+	//}
+
+	void FixedUpdate ()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, slide, jump);
+		jump = false;
+	}
+}
